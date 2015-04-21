86552315e86bc81fb50bb50be9591c7e369a31c7
ypcs03 <yinpengcs03@gmail.com>
2015-04-21 16:38:04 -0400

    Tue Apr 21 16:38:04 EDT 2015

--- CC150/AddWithoutMath.java
+++ CC150/AddWithoutMath.java
@@ -1,11 +1,12 @@
 import java.util.*;
-
+//There is another way in c, using the index of an array:
+//address=(char*)a; &address[b] is the result
 public class AddWithoutMath{
     public static void main(String[]args){
 
         AddWithoutMath daemon = new AddWithoutMath();
 
-        System.out.println(daemon.add(3,4));
+        System.out.println(daemon.add(-3,4));
     }
 
     int add(int a, int b){
